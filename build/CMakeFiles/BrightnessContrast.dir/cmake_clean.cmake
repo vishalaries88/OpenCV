80d91edab13b3673a70595c5cfd2920798781cfc
Vishal Vishal <vishal.vishal@roche.com>
2019-02-06 16:59:32 +0100

    My first commit

---
+++ build/CMakeFiles/BrightnessContrast.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BrightnessContrast.dir/src/BrightnessContrast.cpp.o"
+  "BrightnessContrast.pdb"
+  "BrightnessContrast"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BrightnessContrast.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
